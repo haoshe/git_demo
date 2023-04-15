b304b6a8aa39fbc20b1e3915f68d7a9f465623b0
HaoShe <haoshecn@hotmail.com>
2023-04-15 18:27:46 +0100

    finish the second version

--- src/com/haoshe/gitPractice/GitPractice.java
+++ src/com/haoshe/gitPractice/GitPractice.java
@@ -3,5 +3,6 @@ package com.haoshe.gitPractice;
 public class GitPractice {
     public static void main(String[] args){
         System.out.println("this is the first version.");
+        System.out.println("this is the second version");
     }
 }
