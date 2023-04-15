2de9ce42b87c39d9c74a1619bb41994f80c3a496
HaoShe <haoshecn@hotmail.com>
2023-04-15 21:16:59 +0100

    delete b

--- src/com/haoshe/gitPractice/GitPractice.java
+++ src/com/haoshe/gitPractice/GitPractice.java
@@ -4,11 +4,7 @@ public class GitPractice {
     public static void main(String[] args){
         System.out.println("this is the first version.");
         System.out.println("this is the second version");
-        System.out.println("A did something here");
-        System.out.println("B did something here");
-        System.out.println("B did another thing");
-        System.out.println("A did another thing");
-        System.out.println("B did yet another thing");
         System.out.println("create a branch");
+
     }
 }
