3d5cc8eddeb210bb69e3acea3b67eea5aeadfe2f
HaoShe <haoshecn@hotmail.com>
2023-04-15 18:41:58 +0100

    B finish something

--- src/com/haoshe/gitPractice/GitPractice.java
+++ src/com/haoshe/gitPractice/GitPractice.java
@@ -4,5 +4,6 @@ public class GitPractice {
     public static void main(String[] args){
         System.out.println("this is the first version.");
         System.out.println("this is the second version");
+        System.out.println("B did something here");
     }
 }
